1f778cb80ab416ef38ed9c1fdde201ee784e64d4
Aleksey Karlov <all-kas@yandex.ru>
2024-12-05 20:49:17 +0300

    fix 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.SQLException;
@@ -10,18 +11,18 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         Util.getConnection();
-        UserDao UserService = new UserDaoHibernateImpl();
+        UserServiceImpl userService = new UserServiceImpl();
 
-        UserService.createUsersTable();
+        userService.createUsersTable();
 
-        UserService.saveUser("Name1", "LastName1", (byte) 20);
-        UserService.saveUser("Name2", "LastName2", (byte) 25);
-        UserService.saveUser("Name3", "LastName3", (byte) 31);
-        UserService.saveUser("Name4", "LastName4", (byte) 38);
-        UserService.getAllUsers();
-        UserService.removeUserById(1);
-        UserService.cleanUsersTable();
-        UserService.dropUsersTable();
+        userService.saveUser("Name1", "LastName1", (byte) 20);
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name4", "LastName4", (byte) 38);
+        userService.getAllUsers();
+        userService.removeUserById(1);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 
